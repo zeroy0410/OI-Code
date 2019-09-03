2fc1d74f3104bb6869caeb9e83427ce2105d6041
Your Name <you@example.com>
2019-09-03 22:18:04 +0800

    Update

--- Test/biaoda.cpp
+++ Test/biaoda.cpp
@@ -1,93 +1,76 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define M 805
-const int dx[]={0,0,1,-1};
-const int dy[]={1,-1,0,0};
-bool tomin(double& x,double y){
-    if(x>y){x=y;return 1;}
-    return 0;
-}  
-int n;
-int X[M],szx;
-int Y[M],szy;
-int mp[M][M];
-struct Edge{
-    int x1,x2,y1,y2;
-    void Rd(){
-        scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
-        x1<<=1; x2<<=1; y1<<=1; y2<<=1;
-        X[++szx]=x1; X[++szx]=x2; X[++szx]=x1-1; X[++szx]=x2+1;
-        Y[++szy]=y1; Y[++szy]=y2; Y[++szy]=y1-1; Y[++szy]=y2+1;
+typedef pair<int,int>P;
+const int N=100010,M=262150,inf=1000000010;
+int n,m,i,x,y,ans;
+P xmi[M],xma[M],ymi[M],yma[M];
+void build(int x,int a,int b){
+    if(a==b){
+        scanf("%d%d",&xmi[x].first,&ymi[x].first);
+        xmi[x].second=ymi[x].second=a;
+        xma[x]=xmi[x];
+        yma[x]=ymi[x];
+        return;
     }
-    void uni(){
-        x1=lower_bound(X+1,X+szx+1,x1)-X;
-        x2=lower_bound(X+1,X+szx+1,x2)-X;
-        y1=lower_bound(Y+1,Y+szy+1,y1)-Y;
-        y2=lower_bound(Y+1,Y+szy+1,y2)-Y;
-    }
-    void cvr(){
-        for(int i=x1;i<=x2;i++)
-            for(int j=y1;j<=y2;j++)
-                mp[i][j]++;
-    }
-}A[M];
-struct node{
-    int x,y;
-    double d;
-    bool operator <(const node &A)const{
-        return d>A.d;
-    }
-};
-priority_queue<node>q;
-double dis[M][M];
-bool vis[M][M];
-double Dis(int x1,int y1,int x2,int y2){
-    return (double)(abs(X[x1]-X[x2])+abs(Y[y1]-Y[y2]))/min(mp[x1][y1],mp[x2][y2]);
+    int mid=(a+b)>>1;
+    build(x<<1,a,mid),build(x<<1|1,mid+1,b);
+    xmi[x]=min(xmi[x<<1],xmi[x<<1|1]);
+    xma[x]=max(xma[x<<1],xma[x<<1|1]);
+    ymi[x]=min(ymi[x<<1],ymi[x<<1|1]);
+    yma[x]=max(yma[x<<1],yma[x<<1|1]);
 }
-void Dijk(){
-    for(int i=1;i<=szx;i++)
-        for(int j=1;j<=szy;j++){
-        dis[i][j]=1e100;
-        vis[i][j]=0;
-    }
-    dis[A[0].x1][A[0].y1]=0;
-    while(!q.empty())q.pop();
-    q.push((node){A[0].x1,A[0].y1,0});
-    while(!q.empty()){
-        node tmp=q.top();q.pop();
-        int x=tmp.x,y=tmp.y;
-        if(vis[x][y])continue;
-        vis[x][y]=1;
-        if(x==A[0].x2&&y==A[0].y2){
-            printf("%.5lf\n",dis[x][y]/2);
-            return;
-        }
-        for(int i=0;i<4;i++){
-            int xx=x+dx[i];
-            int yy=y+dy[i];
-            if(mp[xx][yy]&&tomin(dis[xx][yy],dis[x][y]+Dis(x,y,xx,yy)))
-                q.push((node){xx,yy,dis[xx][yy]});
-        }
-    }
+P askxmi(int x,int a,int b,int c,int d){
+    if(c>d)return P(inf,0);
+    if(c<=a&&b<=d)return xmi[x];
+    int mid=(a+b)>>1;
+    P t(inf,0);
+    if(c<=mid)t=askxmi(x<<1,a,mid,c,d);
+    if(d>mid)t=min(t,askxmi(x<<1|1,mid+1,b,c,d));
+    return t;
+}
+P askymi(int x,int a,int b,int c,int d){
+    if(c>d)return P(inf,0);
+    if(c<=a&&b<=d)return ymi[x];
+    int mid=(a+b)>>1;
+    P t(inf,0);
+    if(c<=mid)t=askymi(x<<1,a,mid,c,d);
+    if(d>mid)t=min(t,askymi(x<<1|1,mid+1,b,c,d));
+    return t;
+}
+P askxma(int x,int a,int b,int c,int d){
+    if(c>d)return P(-inf,0);
+    if(c<=a&&b<=d)return xma[x];
+    int mid=(a+b)>>1;
+    P t(-inf,0);
+    if(c<=mid)t=askxma(x<<1,a,mid,c,d);
+    if(d>mid)t=max(t,askxma(x<<1|1,mid+1,b,c,d));
+    return t;
+}
+P askyma(int x,int a,int b,int c,int d){
+    if(c>d)return P(-inf,0);
+    if(c<=a&&b<=d)return yma[x];
+    int mid=(a+b)>>1;
+    P t(-inf,0);
+    if(c<=mid)t=askyma(x<<1,a,mid,c,d);
+    if(d>mid)t=max(t,askyma(x<<1|1,mid+1,b,c,d));
+    return t;
+}
+inline int cal(int x,int y,int z){
+    return max(
+    max(askxma(1,1,n,x,z-1).first,askxma(1,1,n,z+1,y).first)-min(askxmi(1,1,n,x,z-1).first,askxmi(1,1,n,z+1,y).first)
+    ,
+    max(askyma(1,1,n,x,z-1).first,askyma(1,1,n,z+1,y).first)-min(askymi(1,1,n,x,z-1).first,askymi(1,1,n,z+1,y).first)
+    );
 }
 int main(){
-    int T;
-    scanf("%d",&T);
-    while(T--){
-        szx=szy=0;
-        scanf("%d",&n);
-        for(int i=1;i<=n;i++)A[i].Rd();A[0].Rd();
-        sort(X+1,X+szx+1);
-        szx=unique(X+1,X+szx+1)-X-1;
-        sort(Y+1,Y+szy+1);
-        szy=unique(Y+1,Y+szy+1)-Y-1;
-        for(int i=0;i<=n;i++)A[i].uni();
-        memset(mp,0,sizeof(mp));
-        for(int i=1;i<=szx;i++)
-            for(int j=1;j<=szy;j++)
-                mp[i][j]=1;
-        for(int i=1;i<=n;i++)A[i].cvr();
-        Dijk();
+    scanf("%d%d",&n,&m);
+    build(1,1,n);
+    while(m--){
+        scanf("%d%d",&x,&y);
+        ans=cal(x,y,askxmi(1,1,n,x,y).second);
+        ans=min(ans,cal(x,y,askxma(1,1,n,x,y).second));
+        ans=min(ans,cal(x,y,askymi(1,1,n,x,y).second));
+        ans=min(ans,cal(x,y,askyma(1,1,n,x,y).second));
+        printf("%d\n",ans);
     }
-    return 0;
 }
--- Test/test.cpp
+++ Test/test.cpp
@@ -1,6 +1,41 @@
 #include<bits/stdc++.h>
+#define M 100005
 using namespace std;
+int n,q;
+struct node{int x,y;}A[M];
+struct poi{int a,b;};
+struct YD_tree{
+	struct YD{
+		int l,r;
+		poi xmx,xmi,ymx,ymi;
+	}tree[M<<2];
+#define fa tree[p]
+#define lson tree[p<<1]
+#define rson tree[p<<1|1]
+	YD up(YD &a,YD &b){
+		YD c;
+		c.l=a.l;c.r=b.r;
+		c.xmx=max(a.xmx,b.xmx);
+		c.xmi=min(a.xmi,b.xmi);
+		c.ymx=max(a.ymx,b.ymx);
+		c.ymi=min(a.ymi,b.ymi);
+		return c;
+	}
+	void build(int l,int r,int p){
+		fa.l=l;fa.r=r;
+		if(l==r){
+			fa.xmi=fa.xmx=A[i].x;
+			fa.ymi=fa.ymx=A[i].y;
+			return;
+		}
+		int mid=(l+r)>>1;
+		build(l,mid,p<<1);
+		build(mid+1,r,p<<1|1);
+	}
+
 int main(){
-	puts("Hello World!");
+	scanf("%d%d",&n,&q);
+	for(int i=1;i<=n;i++)
+		scanf("%d%d",&A[i].x,&A[i].y);
 	return 0;
 }
