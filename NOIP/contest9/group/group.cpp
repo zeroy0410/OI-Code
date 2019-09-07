5ffee56b85b16bdc7112a9a49dfbda6026bee44c
Your Name <you@example.com>
2019-09-07 15:55:40 +0800

    Update

--- NOIP/contest9/group/group.cpp
+++ NOIP/contest9/group/group.cpp
@@ -10,7 +10,7 @@ void read(int &x){
     while(c=getchar(),c>='0'&&c<='9');
     x*=f;
 }
-int n,m,ans[M],h[M],tt,now[M];
+int n,m,ans[M],h[M],tt;
 struct edge{
 	int nxt,to;
 }G[M*3];
@@ -18,70 +18,28 @@ void Add(int a,int b){
 	G[++tt]=(edge){h[a],b};
 	h[a]=tt;
 }
-struct Pbl{
-	int ans[25];
-	void solve(){
-		for(int i=0;i<1<<n;i++){
-			for(int j=1;j<=n;j++){
-				if(i&1<<j-1)ans[j]=2;
-				else ans[j]=1;
-			}
-			bool fl=1;
-			for(int j=1;j<=n;j++){
-				int c=0;
-				for(int k=h[j];k;k=G[k].nxt){
-					int u=G[k].to;
-					if(ans[u]==ans[j])c++;
-				}
-				if(c>1){fl=0;break;}
-			}
-			if(fl){
-				for(int j=1;j<=n;j++)
-					printf("%d ",ans[j]);
-				cout<<endl;
-				return;
-			}
-		}
-		puts("-1");
-	}
-}pbl;
+bool check(int x){
+	int c=0;
+	for(int i=h[x];i;i=G[i].nxt)if(ans[G[i].to]==ans[x])c++;
+	return c>1;
+}
+queue<int>Q;
 int main(){
-	freopen("group.in","r",stdin);
-	freopen("group.out","w",stdout);
+	srand(19260817);
 	read(n);read(m);
 	for(int i=1,a,b;i<=m;i++){
 		read(a);read(b);
 		Add(a,b);Add(b,a);
 	}
-	for(int i=1;i<=n;i++){
-		int c1=0,c2=0,n1=0,n2=0;
-		for(int j=h[i];j;j=G[j].nxt){
-			int u=G[j].to;
-			if(ans[u]==1)c1++,n1=u;
-			else if(ans[u]==2)c2++,n2=u;
-		}
-		if(c1==0){
-			ans[i]=1;
-			now[i]=0;
-		}
-		else if(c2==0){
-			ans[i]=2;
-			now[i]=0;
-		}
-		else if(c1==1&&now[n1]==0){
-			now[n1]++;
-			ans[i]=1;
-			now[i]=1;
-		}
-		else if(c2==1&&now[n2]==0){
-			now[n2]++;
-			ans[i]=2;
-			now[i]=1;
-		}
-		else {puts("-1");return 0;}
+	for(int i=1;i<=n;i++)ans[i]=(rand()&1)+1;
+	for(int i=1;i<=n;i++)if(check(i))Q.push(i);
+	while(!Q.empty()){
+		int x=Q.front();Q.pop();
+		if(!check(x))continue;
+		ans[x]=3-ans[x];
+		for(int i=h[x];i;i=G[i].nxt)if(check(G[i].to))Q.push(G[i].to);
 	}
 	for(int i=1;i<=n;i++)
 		printf("%d ",ans[i]);
-	puts("");
 	return 0;
 }
--- NOIP/contest9/tree/tree.cpp
+++ NOIP/contest9/tree/tree.cpp
@@ -1,41 +1,42 @@
 #include<bits/stdc++.h>
 #define M 3000005
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define max(a,b) (a>b?a:b)
 #define LL long long
 using namespace std;
 const LL mod=1e9+7;
 const LL inv2=500000004;
-void read(int &x){
+int read(){
     char c;
-    x=0;
+    int x=0;
     int f=1;
     while(c=getchar(),c<'0'||c>'9')if(c=='-')f=-1;
     do x=(x<<3)+(x<<1)+(c^48);
     while(c=getchar(),c>='0'&&c<='9');
     x*=f;
+	return x;
 }
 int n,L,R,h[M],tt;
 struct edge{
 	int nxt,to;
 }G[M];
 void Add(int a,int b){
-	G[++tt]=(edge){h[a],b};
+	++tt;G[tt].nxt=h[a];G[tt].to=b;
 	h[a]=tt;
 }
 LL dp[M];
 int dep[M],sz[M];
 LL val[M];
-LL calc(int x,int y){
-	return ((val[x]-1LL*y*(n-y-1)%mod-y)%mod+mod)%mod;
-}
-void dfs(int x,int d){
-	sz[x]=1;dep[x]=d;
+#define calc(x,y) (((val[x]-1LL*y*(n-y-1)%mod-y)%mod+mod)%mod)
+#define u G[i].to
+void dfs(int x){
+	sz[x]=1;
 	for(int i=h[x];i;i=G[i].nxt){
-		int u=G[i].to;
-		dfs(u,d+1);
+		dep[u]=dep[x]+1;
+		dfs(u);
 		sz[x]+=sz[u];
 	}
 	for(int i=h[x];i;i=G[i].nxt){
-		int u=G[i].to;
 		val[x]=(val[x]+1LL*sz[u]*(n-sz[u]-1)%mod)%mod;
 	}
 	val[x]=(val[x]+1LL*(n-sz[x])*(sz[x]-1)%mod)%mod;
@@ -43,29 +44,30 @@ void dfs(int x,int d){
 	val[x]=(val[x]+n-1)%mod;
 }
 LL ret[M],ans,ret2[M];
+LL res,nowc;
 void redfs(int x){
 	if(dep[x]-L>=1){
-		LL res=((ret[dep[x]-L]-ret[max(0,dep[x]-R-1)])%mod+mod)%mod;
-		LL nowc=calc(x,n-sz[x]);
+		res=((ret[dep[x]-L]-ret[max(0,dep[x]-R-1)])%mod+mod)%mod;
+		nowc=calc(x,n-sz[x]);
 		ans=(ans+4*res*nowc%mod)%mod;
 		ans=(ans+2*nowc*(dep[x]-L-max(0,dep[x]-R-1))%mod)%mod;
 		ans=(ans+2*res%mod)%mod;
 	}
 	for(int i=h[x];i;i=G[i].nxt){
-		int u=G[i].to;
 		ret[dep[x]]=(ret[dep[x]-1]+calc(x,sz[u]))%mod;
 		redfs(u);
 	}
 }
 int main(){
-	freopen("tree.in","r",stdin);
-	freopen("tree.out","w",stdout);
+//	freopen("tree.in","r",stdin);
+//	freopen("tree.out","w",stdout);
 	scanf("%d%d%d",&n,&L,&R);
 	for(int i=2,x;i<=n;i++){
-		read(x);
+		x=read();
 		Add(x,i);
 	}
-	dfs(1,1);
+	dep[1]=1;
+	dfs(1);
 	redfs(1);
 	printf("%lld\n",ans*inv2%mod);
 	return 0;
