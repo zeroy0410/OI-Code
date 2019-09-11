37281772c3910f6e8ba48869f66de15e40237692
Your Name <you@example.com>
2019-09-11 21:46:14 +0800

    Update

---
+++ zy/P.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+const int mod=1e9+7;
+int T,n,K,L;
+int main(){
+	scanf("%d",&T);
+	while(T--){
+		scanf("%d%d%d",&n,&K,&L);
+
+	}
+	return 0;
+}
