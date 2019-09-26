8e643ea2ee2564c02210a047d7238930e7d78f37
zryabc <2020150384@qq.com>
2019-09-26 19:55:20 +0800

    Update

---
+++ sxdp/D.cpp
@@ -0,0 +1,8 @@
+#include<cstdio>
+#include<algorithm>
+#include<string.h>
+using namespace std;
+int main(){
+
+	return 0;
+}
--- sxdp/biaoda.cpp
+++ sxdp/biaoda.cpp
@@ -1,66 +1,37 @@
-#include<stdio.h>
-#include<string.h>
+#include<cstdio>
 #include<algorithm>
-#include<iostream>
-#define N 110000
-#define M 210000
+#define M 1055
 using namespace std;
-int nxt[M<<1],to[M<<1],head[N],tot=1;
-int deep[N],fa[N],good[N],bad[N],gc,bc;
-bool vis[N];
-void add(int x,int y)
-{
-	to[++tot]=y;
-	nxt[tot]=head[x];
-	head[x]=tot;
+int n,m;
+int gh[M][15],pt[M][15],f[M][M],vis[15];
+void dfs(int x,int y){
+	for(int i=0;i<=1<<y;i++) f[x][i]=0;
+	if(!y){
+		for(int i=1;i<=n;i++) 
+			if(vis[i])f[x][1]+=gh[x][i]; 
+			else f[x][0]+=pt[x][i];
+		return;
+	}
+	vis[y]=0,dfs(x<<1,y-1),dfs(x<<1|1,y-1);
+	for(int i=0;i<=1<<(y-1);i++) 
+		for(int j=0;j<=1<<(y-1);j++) 
+			f[x][i+j]=max(f[x][i+j],f[x<<1][i]+f[x<<1|1][j]);
+	vis[y]=1,dfs(x<<1,y-1),dfs(x<<1|1,y-1);
+	for(int i=0;i<=1<<(y-1);i++) 
+		for(int j=0;j<=1<<(y-1);j++)
+			f[x][i+j]=max(f[x][i+j],f[x<<1][i]+f[x<<1|1][j]);
 }
-void dfs(int x,int pre)
-{
-	deep[x]=deep[fa[x]]+1,vis[x]=1;
-	int i;
-	for(i=head[x];i;i=nxt[i])
-	{
-		if((i^1)==pre)	continue;
-		if(!vis[to[i]])
-		{
-			fa[to[i]]=x;
-			dfs(to[i],i);
-			good[x]+=good[to[i]];
-			bad[x]+=bad[to[i]];
-		}
-		else
-		{
-			if(deep[to[i]]>deep[x])	continue;
-			printf("%d %d %d\n",(deep[x]-deep[to[i]]&1),x,to[i]);
-			if(deep[x]-deep[to[i]]&1)
-				good[x]++,good[to[i]]--,gc++;
-			else {
-				bad[x]++,bad[to[i]]--,bc++;
-			}
-		}
-	}
-}
-int main()
-{
-	int n,m,i,j,k,x,y,ans;
-	scanf("%d%d",&n,&m);
-	for(i=1;i<=m;i++)
-	{
-		scanf("%d%d",&x,&y);
-		add(x,y),add(y,x);
-	}
-	for(i=1;i<=n;i++)
-	{
-		if(!vis[i])
-			dfs(i,0);
-	}
-	for(i=1,ans=0;i<=n;i++)
-	{
-		if(fa[i]&&bad[i]==bc&&!good[i])
-			ans++;
-	}
-	if(bc==1)
-		ans++;
+int main(){
+	int ans=0;
+	scanf("%d%d",&n,&m);n--;
+	for(int i=0;i<(1<<n);i++) 
+		for(int j=1;j<=n;j++)
+			scanf("%d",&gh[i+(1<<n)][j]);
+	for(int i=0;i<(1<<n);i++) 
+		for(int j=1;j<=n;j++)
+			scanf("%d",&pt[i+(1<<n)][j]);
+	dfs(1,n);
+	for(int i=0;i<=m;++i)ans=max(ans,f[1][i]);
 	printf("%d\n",ans);
 	return 0;
 }
