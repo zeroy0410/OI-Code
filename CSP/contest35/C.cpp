0c1e36a9668a52399cdaae3a60c65cff13d70fcd
zryabc <2020150384@qq.com>
2019-10-25 16:35:01 +0800

    Update

--- CSP/contest35/C.cpp
+++ CSP/contest35/C.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 #define M 105
+#define LL long long
 const int mod=998244353;
 using namespace std;
-int n,m,p,q,C[M][M],S[M][M],fac[M];
-void Add(int& x,int y){
+int n,m,p,q;
+LL C[M][M],S[M][M],fac[M];
+void Add(LL& x,LL y){
 	x+=y;
 	if(x>=mod)x-=mod;
 	if(x<0)x+=mod;
@@ -18,19 +20,18 @@ void Init(){
 	S[0][0]=1;
 	for(int i=1;i<M;i++){
 		for(int j=1;j<=i;j++)
-			S[i][j]=(1LL*S[i-1][j]*j+S[i-1][j-1])%mod;
+			S[i][j]=(1LL*S[i-1][j]*j%mod+S[i-1][j-1])%mod;
 	}
 	fac[0]=1;
 	for(int i=1;i<M;i++)
 		fac[i]=1LL*fac[i-1]*i%mod;
 }
 struct P1{
-	int dp[M][M];
+	LL dp[M][M];
 	void solve(){
 		memset(dp,0,sizeof(dp));
-		for(int i=1;i<=p;i++){
-			dp[1][i]=1LL*C[p][i]*S[n][i]*fac[i]%mod;
-		}
+		for(int i=1;i<=p;i++)
+			dp[1][i]=1LL*C[p][i]*S[n][i]%mod*fac[i]%mod;
 		for(int i=2;i<=m;i++){
 			for(int j=1;j<=p;j++){
 				for(int k=0;k<=j;k++){//same
@@ -40,14 +41,15 @@ struct P1{
 				}
 			}
 		}
-		int ans=0;
+		LL ans=0;
 		for(int i=1;i<=p;i++)
 			Add(ans,dp[m][i]);
-		printf("%d\n",ans);
+		printf("%lld\n",ans);
 	}
 }p1;
 struct Matrix{
-    int n,m,a[M][M];
+    int n,m;
+	LL a[M][M];
     void clear(){memset(a,0,sizeof(a));}
     void resize(int _n,int _m){n=_n;m=_m;}
     Matrix operator *(const Matrix &_)const{
@@ -63,12 +65,45 @@ struct Matrix{
         }
         return res;
     }
-}Ma,F_d;
+};
+Matrix qkpow(Matrix a,int b){
+	Matrix res;res.resize(p,p);
+	for(int i=1;i<=p;i++)
+		res.a[i][i]=1;
+	while(b){
+		if(b&1)res=res*a;
+		a=a*a;
+		b>>=1;
+	}
+	return res;
+}
+struct P2{
+	Matrix trans,Ans;
+	void solve(){
+		trans.clear();Ans.clear();
+		trans.resize(p,p);Ans.resize(1,p);
+		for(int i=1;i<=p;i++)
+			Ans.a[1][i]=1LL*C[p][i]*S[n][i]%mod*fac[i]%mod;
+		for(int j=1;j<=p;j++){
+			for(int k=0;k<=j;k++){//same
+				for(int r=0;r<=p-j;r++)//unsame
+					if(r+j>=q)
+						Add(trans.a[j][k+r],1LL*C[j][k]*C[p-j][r]%mod*S[n][k+r]%mod*fac[k+r]%mod);
+			}
+		}
+		Ans=Ans*qkpow(trans,m-1);
+		LL ans=0;
+		for(int i=1;i<=p;i++)
+			Add(ans,Ans.a[1][i]);
+		printf("%lld\n",ans);
+	}
+
+}p2;
 int main(){
 //	freopen("color.in","r",stdin);
 //	freopen("color.out","w",stdout);
 	Init();
 	scanf("%d%d%d%d",&n,&m,&p,&q);
-	p1.solve();
+	p2.solve();
 	return 0;
 }
--- bz/A.cpp
+++ bz/A.cpp
@@ -1,6 +1,36 @@
 #include<bits/stdc++.h>
+#define M 205
 using namespace std;
+int n,T,s,e,G[M][M][21],Q[M],qc;
+struct in{
+	int l,a,b;
+}In[M];
+void Floyd(){
+	for(int r=1;r<=20;r++)
+		for(int k=1;k<=qc;k++)
+			for(int i=1;i<=qc;i++)
+				for(int j=1;j<=qc;j++)
+					G[i][j][r]=min(G[i][j][r],G[i][k][r-1]+G[k][j][r-1]);
+}
+int dp[M];
 int main(){
-
+	scanf("%d%d%d%d",&n,&T,&s,&e);Q[++qc]=s;Q[++qc]=e;
+	memset(G,0x3f,sizeof(G));
+	for(int i=1;i<=T;i++){
+		scanf("%d%d%d",&In[i].l,&In[i].a,&In[i].b);
+		Q[++qc]=a;Q[++qc]=b;
+	}
+	sort(Q+1,Q+qc+1);
+	qc=unique(Q+1,Q+qc+1)-Q-1;
+	for(int i=1,a,b,l;i<=T;i++){
+		In[i].a=lower_bound(Q+1,Q+qc+1,In[i].a)-Q;
+		In[i].b=lower_bound(Q+1,Q+qc+1,In[i].b)-Q;
+		a=In[i].a,b=In[i].b,c=In[i].l;
+		G[a][b][0]=G[b][a][0]=min(G[a][b][0],c);
+	}
+	s=lower_bound(Q+1,Q+qc+1,s)-Q;
+	e=lower_bound(Q+1,Q+qc+1,e)-Q;
+	Floyd();
+	dp[s][0]=0;
 	return 0;
 }
