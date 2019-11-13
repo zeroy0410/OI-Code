a3a79eacf57964064d6066c24eb48bfbd76d75ef
zryabc <2020150384@qq.com>
2019-11-13 15:49:48 +0800

    Update

---
+++ CSP/contest51/traffic/biaoda.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<cstdio>
+#include<iomanip>
+#include<algorithm>
+#include<cstring>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<map>
+#include<set>
+
+#define ll long long
+#define db double
+#define rg register int
+
+using namespace std;
+
+int n,m,q;
+int d[1000005]; //并查集里面节点到其联通块的根的距离
+int g[1000005]; //当前联通块的gcd，如天天爱跑步
+int fa[1000005]; //并查集
+
+inline int qr(){
+    register char ch; register bool sign=0; rg res=0;
+    while(!isdigit(ch=getchar()))if(ch=='-')sign=1;
+    while(isdigit(ch))res=res*10+(ch^48),ch=getchar();
+    if(sign)return -res; else return res;
+}
+
+inline int gcd(int a,int b){
+    if(b==0)return a; return gcd(b,a%b);
+}
+
+inline int getfa(int x){
+    if(x==fa[x])return x;
+    rg to=getfa(fa[x]); //注意顺序，防止信息被覆盖
+    d[x]=(d[x]+d[fa[x]])%m; //更新当前节点到根的距离
+    return fa[x]=to; //最后再赋值
+}
+
+inline void exgcd(int a,int b,int &x,int &y){ //扩展欧几里得
+    if(b==0) x=1,y=0; //直接构造一组解
+    else exgcd(b,a%b,y,x),y-=a/b*x; //我们不需要求gcd，只要解方程就好
+}
+
+int main(){
+    n=qr(); m=qr(); q=qr();
+    for(rg i=1;i<=n;++i) fa[i]=i,g[i]=m;
+    for(rg i=1;i<=q;++i){
+        rg op=qr(),u=qr(),v=qr(),w=qr();
+        rg fx=getfa(u),fy=getfa(v); //查找根
+        if(op==1){
+            if(fx!=fy){ fa[fx]=fy; //合并
+                d[fx]=((ll)d[u]+d[v]+w)%m; //一个联通块只有一个根，所以另一个需要改距离
+                g[fy]=gcd(gcd(g[fx],g[fy]),2*w); //更新联通块的gcd
+            }else g[fx]=gcd(gcd(g[fx],((ll)d[u]+d[v]+w)%m),2*w); //新加了一个环，合并环的贡献
+        } else{ //请注意大小写！！！！！！
+            rg ans=0,b=qr()%g[fx],c=qr(); //读入
+            if(fx!=fy){puts("0"); continue;} //不连通意味着不能到达
+            rg A=b,B=g[fx],G=gcd(A,B),C=(ll)d[u]+d[v]-w+B; //列出同余方程的系数
+            // 同余方程： C + x*A = 0 (mod B)   ->   x*A + y*B = C (mod g[fx])
+            if(C%G){puts("0"); continue;} //方程无解，请参见扩欧的做法
+            rg x,y; A/=G; B/=G; C/=G; //请参见扩欧的解方程的步骤
+            exgcd(A,B,x,y); x=((ll)x*C%B+B)%B; //解出同余方程
+            if(x<c)ans=(c-1-x)/B+1; //注意我们找的是x的最小解，循环长度是模数B！ （同余方程性质）
+            printf("%d\n",ans); //输出答案
+        }
+    }
+    return 0;
+}
--- CSP/contest51/traffic/traffic.cpp
+++ CSP/contest51/traffic/traffic.cpp
@@ -2,9 +2,40 @@
 #define M 1000005
 using namespace std;
 int n,m,q;
+int fa[M],dis[M],g[M];
+int gcd(int a,int b){ return b==0?a:gcd(b,a%b); }
+void exgcd(int a,int b,int& x,int& y){
+	if(b==0){x=1,y=0;return;}
+	else exgcd(b,a%b,y,x),y-=a/b*x;
+}
+int getfa(int x){
+	if(fa[x]==x)return x;
+	int to=getfa(fa[x]);
+	dis[x]=(dis[x]+dis[fa[x]])%m;
+	return fa[x]=to;
+}
 int main(){
 //	freopen("traffic.in","r",stdin);
 //	freopen("traffic.out","w",stdout);
 	scanf("%d%d%d",&n,&m,&q);
+	for(int i=1,op,u,v,x,w,b,c;i<=q;i++){
+		scanf("%d",&op);
+		if(op==1){
+			scanf("%d%d%d",&u,&v,&w);
+			int r1=getfa(u),r2=getfa(v);
+			if(fa[r1]!=fa[r2]){
+				fa[r1]=r2;
+				dis[r1]=(1LL*dis[u]+dis[v]+w)%m;
+				g[r1]=gcd(gcd(g[r1],g[r2]),2*w);
+			}
+			else g[f1]=gcd(gcd(g[f1],(1LL*dis[u]+dis[v]+w)%m),2*w);
+		}
+		else {
+			scanf("%d%d%d%d%d",&u,&v,&x,&b,&c);
+			int r1=getfa(u),r2=getfa(v);
+			b%=g[r1];
+			int A=b;
+		}
+	}
 	return 0;
 }
