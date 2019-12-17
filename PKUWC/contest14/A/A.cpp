8ca2537f25bde60dc9c69831a1029806830a4584
zryabc <2020150384@qq.com>
2019-12-17 21:09:38 +0800

    update

--- PKUWC/contest14/A/A.cpp
+++ PKUWC/contest14/A/A.cpp
@@ -1,13 +1,24 @@
 #include<bits/stdc++.h>
+#define LL long long
 #define M 200005
 using namespace std;
-int T,n,m,K,a,A[M];
+int T,n,m,K,a;
+LL A[M];
+bool check(LL x){
+	
+}
 int main(){
 	scanf("%d",&T);
 	while(T--){
 		scanf("%d%d%d%d",&n,&m,&K,&a);
 		for(int i=1;i<=n;i++)scanf("%d",&A[i]);
-		
+		LL l=1,r=1e18,ans=-1;
+		while(l<=r){
+			LL mid=(l+r)>>1;
+			if(check(mid))ans=mid,l=mid+1;
+			else r=mid-1;
+		}
+		printf("%lld\n",ans);
 	}
 	return 0;
 }
