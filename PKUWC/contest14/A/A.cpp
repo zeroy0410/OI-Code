2cb23fd3f68555790ce4e3d1536b71ad6d5cd3c4
zryabc <2020150384@qq.com>
2019-12-17 22:00:54 +0800

    update

--- PKUWC/contest14/A/A.cpp
+++ PKUWC/contest14/A/A.cpp
@@ -4,14 +4,35 @@
 using namespace std;
 int T,n,m,K,a;
 LL A[M];
+vector<int>wk[M];
+priority_queue<int>Q;
+int ret[M];
 bool check(LL x){
-	
+	while(!Q.empty())Q.pop();
+	int now=0,cnt=0;
+	memset(ret,0,sizeof(ret));
+	for(int i=1;i<=n;i++){
+		for(int j=0;j<wk[i].size();j++)Q.push(wk[i][j]);
+		now+=ret[i];
+		while(now+A[i]<x){
+			if(Q.empty())return 0;
+			int u=Q.top();Q.pop();
+			if(u<i)continue;
+			now+=a;ret[u+1]-=a;cnt++;
+		}
+	}
+	return cnt<=K;
 }
 int main(){
 	scanf("%d",&T);
 	while(T--){
 		scanf("%d%d%d%d",&n,&m,&K,&a);
-		for(int i=1;i<=n;i++)scanf("%d",&A[i]);
+		for(int i=1;i<=n;i++)wk[i].clear();
+		for(int i=1;i<=n;i++)scanf("%lld",&A[i]);
+		for(int i=1,l,r;i<=m;i++){
+			scanf("%d%d",&l,&r);
+			wk[l].push_back(r);
+		}
 		LL l=1,r=1e18,ans=-1;
 		while(l<=r){
 			LL mid=(l+r)>>1;
