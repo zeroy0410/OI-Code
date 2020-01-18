bbb9b8551ce61ab0e23654fba3df5266fe3c32bc
zryabc <2020150384@qq.com>
2020-01-18 21:14:51 +0800

    Update

---
+++ ZJOI2019/D/D.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+const int mod=998244353;
+using namespace std;
+int n,S[105],P[105];
+int dp[2][105];
+int main(){
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)scanf("%d",&S[i]);
+	for(int i=1;i<=n;i++)scanf("%d",&P[i]);
+
+	return 0;
+}
